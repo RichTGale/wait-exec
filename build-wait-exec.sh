591a534978b64408ff397af20c05e73f1b7ad23b
richtgale <richtgale@gmail.com>
2025-09-19 20:53:04 +1000

    Trying to fix a weird problemm.

--- build-wait-exec.sh
+++ build-wait-exec.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-printf "gcc -o mycutils/mycutils.h clocks/timer_sec/timer_sec.h mycutils/mycutils.c clocks/timer_sec/timer_sec.c wait_exec.c\n"
-gcc -o mycutils/mycutils.h clocks/timer_sec/timer_sec.h mycutils/mycutils.c clocks/timer_sec/timer_sec.c wait_exec.c
+printf "gcc -o mycutils.h timer_sec.h mycutils.c timer_sec.c wait_exec.c\n"
+gcc -o mycutils.h timer_sec.h mycutils.c timer_sec.c wait_exec.c
